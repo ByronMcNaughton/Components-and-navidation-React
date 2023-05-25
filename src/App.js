807f234e9d97c2586b9826064126ecb49d8407bf
Byron McNaughton <mcbyron99@gmail.com>
2023-05-25 10:17:51 +0100

    Creating Button component / tailwind css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import Button from "./components.js/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
 function App() {
-  return <div>App</div>;
+  const handleClick = () => {
+    console.log("click...");
+  };
+  return (
+    <div>
+      <div>
+        <Button primary onClick={handleClick}>
+          <GoBell />
+          Button 1
+        </Button>
+      </div>
+      <div>
+        <Button secondary>
+          <GoCloudDownload />
+          Button 2
+        </Button>
+      </div>
+      <div>
+        <Button success>
+          <GoDatabase />
+          Button 3
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          Button 4
+        </Button>
+      </div>
+      <div>
+        <Button danger rounded>
+          Button 4
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
